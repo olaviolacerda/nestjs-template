415f3f60fd12c26e005c94706503b77e145d9ebf
Olavio <olavio.lacerda@hotmail.com>
2024-02-28 15:35:20 -0300

    chore: change swagger config

---
+++ src/configuration/swagger.config.ts
@@ -0,0 +1,11 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export default new DocumentBuilder()
+  .setTitle('NestJS Template example')
+  .setDescription('The API description')
+  .setVersion('1.0')
+  .addBearerAuth(
+    { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+    'access-token',
+  )
+  .build();
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './core/modules/app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { RequestMethod } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import swaggerDocumentOptions from './configuration/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.setGlobalPrefix('api/v1', {
-    exclude: [{ path: 'health', method: RequestMethod.GET }],
-  });
+  app.setGlobalPrefix('api/v1');
 
-  const config = new DocumentBuilder()
-    .setTitle('NestJS Template example')
-    .setDescription('The API description')
-    .setVersion('1.0')
-    .addBearerAuth(
-      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
-      'access-token',
-    )
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerDocumentOptions);
   SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
