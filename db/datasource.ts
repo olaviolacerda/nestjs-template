e7fdfa8d03c973fe8cb0ace1bb0058bf486b2fd1
Olavio <olavio.lacerda@hotmail.com>
2024-02-29 12:14:57 -0300

    chore: organize db folder

--- db/datasource.ts
+++ db/datasource.ts
@@ -1,5 +1,6 @@
-import dbConfig from '../src/configuration/db.config';
 import { DataSource } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+import dbConfig from '../src/configurations/db.config';
 
 export default new DataSource(dbConfig as PostgresConnectionOptions);
