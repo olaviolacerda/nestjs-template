c65c669a874a7d4c0f6395916a0dacc5361e0edc
Olavio <olavio.lacerda@hotmail.com>
2024-03-01 22:59:31 -0300

    test: add unit tests for strategies

--- test/unit/auth/strategies/local.strategy.spec.ts
+++ test/unit/auth/strategies/local.strategy.spec.ts
@@ -1,3 +1,49 @@
+import { createMock } from '@golevelup/ts-jest';
+
+import { LocalStrategy } from '../../../../src/core/auth/strategies/local.strategy';
+import { AuthService } from '../../../../src/core/providers/services/auth.service';
+import { mockUser } from '../../mocks/users.mock';
+
 describe('LocalStrategy', () => {
-  it.todo('should be defined');
+  let authService: AuthService;
+  let strategy: LocalStrategy;
+
+  beforeEach(() => {
+    authService = createMock<AuthService>();
+    strategy = new LocalStrategy(authService);
+  });
+
+  it('should be defined', () => {
+    expect(strategy).toBeDefined();
+  });
+
+  it('should validate a valid user', async () => {
+    const credentials = { username: 'username', password: 'password' };
+    const spyValidateUser = jest
+      .spyOn(authService, 'validateUser')
+      .mockResolvedValue(mockUser);
+
+    const user = await strategy.validate(
+      credentials.username,
+      credentials.password,
+    );
+
+    expect(spyValidateUser).toHaveBeenCalled();
+    expect(user).toEqual(mockUser);
+  });
+
+  it('should throw if an invalid user is provided', async () => {
+    const credentials = { username: 'username', password: 'password' };
+    const spyValidateUser = jest
+      .spyOn(authService, 'validateUser')
+      .mockResolvedValue(null);
+
+    try {
+      await strategy.validate(credentials.username, credentials.password);
+    } catch (error) {
+      expect(spyValidateUser).toHaveBeenCalled();
+      expect(error).toBeDefined();
+      expect(error.message).toBe('Unauthorized');
+    }
+  });
 });
